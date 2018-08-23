6a488be852ced05c82cf760f15432293ea9d15cb
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-08-23 11:04:40 +0530

    Create 87.java

---
+++ 87.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,b,c=0;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		for(int i=1;i<=a&&i<=b;i++)
+		{
+			if(a%i==0&&b%i==0)
+			{
+				c=i;
+			}
+		}
+		System.out.print(c);// your code goes here
+	}
+}
